65a9edb2f0881f61852fd2a60d65affb43625cc4
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2023-12-26 09:42:19 +0530

    updated bcp query

--- Misscellaneous Queries/BCP-QueryOut.sql
+++ Misscellaneous Queries/BCP-QueryOut.sql
@@ -1,5 +1,6 @@
 bcp  "exec DBA.dbo.WhoIsActive_ResultSets" queryout "G:\Performance-Issues\MyDbServerName\WhoIsActive_ResultSets.txt" -w -C OEM -t"," -T -SMyDbServerName
 
-BCP "select * from DBA.[dbo].[commandlog_stage_testing_TestCase01]" queryout "D:\commandlog_stage_testing_TestCase01.dat" -S MSI -T -c -t"!~!"
+BCP "select * from dbo.vw_performance_counters pc where pc.collection_time_utc between dateadd(day,-16,getutcdate()) and dateadd(day,-1,getutcdate())" `
+        queryout "T:\DBA_Network_Test_Using_BCP.dat" -S SqlPractice -d DBA -a 65535 -T -c -t"!~!"
 
 BCP [$database].dbo.commandlog_stage_testing_TestCase01 in "D:\commandlog_stage_testing_TestCase01.dat" -S msi -T -c -t"!~!"
