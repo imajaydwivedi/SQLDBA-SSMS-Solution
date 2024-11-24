3588b4a1df0644066dbe62b6d4a117781bd1fc15
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2024-11-24 18:01:03 +0530

    updated HumanEvents Query

--- BlitzQueries/Blitz-sp_HumanEvents-Usages.sql
+++ BlitzQueries/Blitz-sp_HumanEvents-Usages.sql
@@ -1,5 +1,12 @@
 /*	https://www.erikdarlingdata.com/sp_humanevents/
 */
+-- Get all queries with duration over 50 ms
+exec dbo.sp_HumanEvents @event_type = 'query',
+			@query_duration_ms = 50,
+			@seconds_sample = 100
+			,@database_name = 'DBA'
+go
+
 -- Get compilations. Run this from SQL Notebooks to save the results
 exec dbo.sp_HumanEvents @event_type = 'compilations', @seconds_sample = 600;
 
