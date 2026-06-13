27ef4ee6738a698d6b1953a401491e9ea2a6d742
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2026-06-13 13:49:16 +0530

    removed extra *.sql files

--- StackOverflow/QA - Low views, high votes yet unanswered.sql
+++
@@ -1,8 +0,0 @@
--- https://data.stackexchange.com/stackoverflow/query/951/low-views-high-votes-yet-unanswered
-
--- Enter Query Title
--- Enter Query Description
-select /* Low views, high votes yet unanswered */
-		top 500 Id as [Post Link], Score, ViewCount from Posts 
-where Score > 2 and ViewCount <> 0 and ParentId is null and AcceptedAnswerId is null
-order by ViewCount asc
